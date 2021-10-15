006dc45d703b84a5f9f02b6b416b0e78ea2a32d5
neryad <dayern.gomez@hotmail.com>
2021-10-15 17:38:37 -0400

    tetstin api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const PORT = 8080;
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
-
+let articles = [];
 const app = express();
 const url = 'https://listindiario.com/';
 axios(url)
@@ -10,13 +10,14 @@ axios(url)
     const html = response.data;
 
     const $ = cheerio.load(html);
-    const articles = [];
+    // const articles = [];
     $('.row_item', html).each(function () {
       const title = $(this).text();
-      const url = $(this).find('a').attr('href');
+      const url = 'https://listindiario.com' + $(this).find('a').attr('href');
       const image = $(this).find('img').attr('src');
       articles.push({ title, url, image });
     });
+
     console.log(articles);
   })
   .catch((err) => console.log(err));
@@ -28,4 +29,10 @@ axios(url)
 //   hora = new Date().toLocaleString();
 //   console.log('Tempo', hora);
 // }
+app.get('/listin', (req, res, next) => {
+  res.status(200).json({
+    ok: true,
+    data: articles,
+  });
+});
 app.listen(PORT, () => console.log(`Server on port: ${PORT}`));
